6038b4f33c9569597e8893e77ec00f6532662539
Kevin Jiang <kjiang95@gmail.com>
2020-07-23 17:58:55 -0500

    pull

--- src/routes/AdoptionRoute/AdoptionRoute.js
+++ src/routes/AdoptionRoute/AdoptionRoute.js
@@ -134,7 +134,7 @@ export default class Adoption extends Component {
         </Button>
 
         <Modal show={this.state.show} onHide={this.handleClose}>
-          <Modal.Header closeButton>
+          <Modal.Header>
             <Modal.Title>Congrats!</Modal.Title>
           </Modal.Header>
           <Modal.Body>You just adopted a friend!</Modal.Body>
